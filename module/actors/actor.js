d7ea0f0cde6b3a2b2ce83204accc39ff4806cbc2
Christophe Laudon <ch.laudon@gmail.com>
2021-08-31 08:43:57 +0200

    Correction nombre PC

--- module/actors/actor.js
+++ module/actors/actor.js
@@ -233,7 +233,7 @@ export class CoCActor extends Actor {
         attributes.init.penalty = - parseInt(protection);
         attributes.init.value = attributes.init.base + attributes.init.bonus + attributes.init.penalty;
 
-        attributes.fp.base = 3 + stats.cha.mod;
+        attributes.fp.base = 2 + stats.cha.mod;
         attributes.fp.bonus = (profile && profile.data.bonuses.fp) ? profile.data.bonuses.fp : 0;
         attributes.fp.max = attributes.fp.base + attributes.fp.bonus;
         attributes.dr.value = attributes.dr.base.value + attributes.dr.bonus.value;
